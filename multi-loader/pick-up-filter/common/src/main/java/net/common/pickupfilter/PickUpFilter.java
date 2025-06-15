7f52031a92a34106a90660c5baa37c97f62dddf9
roaccat <roaccat@outlook.com>
2025-06-15 15:41:12 +0300

    deletes

--- multi-loader/pick-up-filter/common/src/main/java/net/common/pickupfilter/PickUpFilter.java
+++
@@ -1,9 +0,0 @@
-package net.common.pickupfilter;
-
-public class PickUpFilter {
-    public static final String MOD_ID = "pickupfilter";
-
-    public static void init() {
-        System.out.println("PickUpFilter init()");
-    }
-}
